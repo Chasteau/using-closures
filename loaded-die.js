2fc685eab1107b2bcfddbacb6385eb5c7838e509
Chasteau <davidchasteau@gmail.com>
2017-06-01 23:14:39 +0000

    loaded die version statistically valid cheating?

--- loaded-die.js
+++ loaded-die.js
@@ -1,23 +1,38 @@
-var rollDie = function () {
-  return Math.floor(1 + Math.random() * 6);
-}
+// var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+// var loadedDie = list[Math.floor(Math.random() * list.length)];
+// console.log(loadedDie);
+//var Placeholdervalue = myPlaceHolderArray[rand];
 
-console.log(rollDie());  // 1 (for example)
+//console.log(Placeholdervalue);
+
+// var rollDie = function () {
+//   return Math.floor(1 + Math.random() * 6);
+// }
+
+// //console.log(rollDie());  // 1 (for example)
 
 
-// However, instead of a random die, we want to create a
-// cheat die that would let us predict the next number.
-// Create a function called makeLoadedDie() which returns a
-// function that when called, seems to generate random numbers
-// between 1 and 6, but in fact returns numbers in a row from a hardcoded list.
+// // However, instead of a random die, we want to create a
+// // cheat die that would let us predict the next number.
+// // Create a function called makeLoadedDie() which returns a
+// // function that when called, seems to generate random numbers
+// // between 1 and 6, but in fact returns numbers in a row from a hardcoded list.
 
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  var loadedRoll = 0;
 
   return function() {
-    /* your code here */
+    loadedDie = list[Math.floor(Math.random() * list.length)];
+    // console.log(loadedDie);
+    // using map can replace
+
+    // can push
+
+    // can assign a value
+    return loadedDie;
   }
+
 }
 
 var rollLoadedDie = makeLoadedDie();
