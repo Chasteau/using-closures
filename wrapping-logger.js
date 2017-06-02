8e1c9312639686a1ee7317021a9eb10e228365ef
Chasteau <davidchasteau@gmail.com>
2017-06-02 03:35:28 +0000

    wrapping-logger v1 working

---
+++ wrapping-logger.js
@@ -0,0 +1,23 @@
+// Create a function called wrapLog() that takes in a function (callback)
+// and a string (name)
+var wrapLog = function (callback, name) {
+// returns a function that does the same thing as callback
+   return function (){
+    // can refer to the variables
+    // passed into function using the arguments[i]
+    // can use Template literals to print outputs
+    var args = `(${arguments[0]},${arguments[1]})`;
+    var output = callback(arguments[0],arguments[1]);
+    console.log(`${name}${args} => ${output}`);
+  }
+};
+
+// 1,2 -> 3
+var sum = function (a,b) {
+  return a + b;
+};
+
+var logSum = wrapLog(sum, "sum");
+
+logSum(5,3); // sum(5,3) => 8
+logSum(3,2); // sum(3,2) => 5
